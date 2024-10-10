69bb4b3e2388ebbcd88cfcb32b4b18168f6001bd
David Zucker <david@programmableproduction.com>
2024-10-10 08:26:10 +0000

    fix: permission error on socket mounting

--- src/docker-outside-of-docker/install.sh
+++ src/docker-outside-of-docker/install.sh
@@ -6,12 +6,24 @@ set -e
 INSTALLDOCKER=${INSTALLDOCKER:-"true"}
 INSTALLBUILDX=${INSTALLBUILDX:-"true"}
 INSTALLDOCKERCOMPOSE=${INSTALLDOCKERCOMPOSE:-"true"}
+ENABLE_NONROOT_DOCKER=${ENABLE_NONROOT_DOCKER:-"true"}
 SOURCE_SOCKET=/var/run/docker-host.sock
 TARGET_SOCKET=/var/run/docker.sock
+USERNAME="${USERNAME:-"${_REMOTE_USER:-"automatic"}"}"
 
 echo "Activating feature 'docker-outside-of-docker'"
 
 apk update
+
+
+# Checks if packages are installed and installs them if not
+install_if_not() {
+    if [ -z "$(apk list -I "$@")" ]; then
+        echo "Install package $@"
+        apk add --no-cache "$@"
+    fi
+}
+
 # If init file already exists, exit
 if [ -f "/usr/local/share/docker-init.sh" ]; then
     exit 0
@@ -25,24 +37,47 @@ if [ "${INSTALLBUILDX}" = "true" ]; then
     apk --no-cache add docker-cli-buildx
 fi
 
-mkdir -p /usr/local/share
+if [[ $INSTALLDOCKERCOMPOSE == "true" ]]; then
+    apk --no-cache add docker-compose
+fi
 
-if [[ -z $_CONTAINER_USER ]]; then
-    ln -s /var/run/docker-host.sock /var/run/docker.sock
-    echo -e '#!/bin/sh\nexec "$@"' > /usr/local/share/docker-init.sh
-else
+# Install requirement to run that script 
+install_if_not socat
+install_if_not sudo
+install_if_not shadow
 
-apk --no-cache add socat sudo
-echo "$_CONTAINER_USER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$_CONTAINER_USER
-chmod 0440 /etc/sudoers.d/$_CONTAINER_USER
+# Setup a docker group in the event the docker socket's group is not root
+if ! grep -qE '^docker:' /etc/group; then
+    echo "(*) Creating missing docker group..."
+    groupadd --system docker
+fi
+
+cat /etc/passwd
+
+usermod -aG docker "${_REMOTE_USER}"
+
+DOCKER_GID="$(grep -E '^docker:x:[^:]+' /etc/group | cut -d: -f3)"
+
+echo "$_REMOTE_USER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$_REMOTE_USER
+chmod 0440 /etc/sudoers.d/$_REMOTE_USER
 
 mkdir -p "$(dirname "${SOURCE_SOCKET}")"
 touch "${SOURCE_SOCKET}"
 ln -s "${SOURCE_SOCKET}" "${TARGET_SOCKET}"
 
+chown -h "${_REMOTE_USER}":root "${TARGET_SOCKET}"
+
+## Setup entrypoint script
+mkdir -p /usr/local/share
+
 tee /usr/local/share/docker-init.sh > /dev/null \
 << EOF
-#!/bin/sh
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+#-------------------------------------------------------------------------------------------------------------
+# Extracted from https://github.com/devcontainers/features/blob/main/src/docker-outside-of-docker/install.sh
 
 set -e
 
@@ -50,31 +85,54 @@ SOCAT_PATH_BASE=/tmp/vscr-docker-from-docker
 SOCAT_LOG=\${SOCAT_PATH_BASE}.log
 SOCAT_PID=\${SOCAT_PATH_BASE}.pid
 
-SOURCE_SOCKET=$SOURCE_SOCKET
-TARGET_SOCKET=$TARGET_SOCKET
+# Wrapper function to only use sudo if not already root
+sudoIf()
+{
+    if [ "\$(id -u)" -ne 0 ]; then
+        sudo "\$@"
+    else
+        "\$@"
+    fi
+}
 
-if [ ! -f "\$SOCAT_PID" ] || ! kill -0 \$(cat \$SOCAT_PID) 2>/dev/null; then
-    echo "Enabling socket proxy."
-    echo "Proxying ${SOURCE_SOCKET} to ${TARGET_SOCKET} for vscode"
-    sudo rm -rf ${TARGET_SOCKET}
-    (sudo socat UNIX-LISTEN:\$TARGET_SOCKET,fork,mode=660,user=$_CONTAINER_USER,backlog=128 UNIX-CONNECT:\$SOURCE_SOCKET 2>&1 | sudoIf tee -a \${SOCAT_LOG} > /dev/null & echo "\$!" | sudoIf tee \${SOCAT_PID} > /dev/null)
-else
-    echo "Socket proxy already running."    
+# Log messages
+log()
+{
+    echo -e "[\$(date)] \$@" | sudoIf tee -a \${SOCAT_LOG} > /dev/null
+}
+
+echo -e "\n** \$(date) **" | sudoIf tee -a \${SOCAT_LOG} > /dev/null
+log "Ensuring ${_CONTAINER_USER} has access to ${SOURCE_SOCKET} via ${TARGET_SOCKET}"
+
+# If enabled, try to update the docker group with the right GID. If the group is root,
+# fall back on using socat to forward the docker socket to another unix socket so
+# that we can set permissions on it without affecting the host.
+if [ "${ENABLE_NONROOT_DOCKER}" = "true" ] && [ "${SOURCE_SOCKET}" != "${TARGET_SOCKET}" ] && [ "${_CONTAINER_USER}" != "root" ] && [ "${_CONTAINER_USER}" != "0" ]; then
+    SOCKET_GID=\$(stat -c '%g' ${SOURCE_SOCKET})
+    if [ "\${SOCKET_GID}" != "0" ] && [ "\${SOCKET_GID}" != "${DOCKER_GID}" ] && ! grep -E ".+:x:\${SOCKET_GID}" /etc/group; then
+        sudoIf groupmod --gid "\${SOCKET_GID}" docker
+    else
+        # Enable proxy if not already running
+        if [ ! -f "\${SOCAT_PID}" ] || ! ps -p \$(cat \${SOCAT_PID}) > /dev/null; then
+            log "Enabling socket proxy."
+            log "Proxying ${SOURCE_SOCKET} to ${TARGET_SOCKET} for vscode"
+            sudoIf rm -rf ${TARGET_SOCKET}
+            (sudoIf socat UNIX-LISTEN:${TARGET_SOCKET},fork,mode=660,user=${_CONTAINER_USER},backlog=128 UNIX-CONNECT:${SOURCE_SOCKET} 2>&1 | sudoIf tee -a \${SOCAT_LOG} > /dev/null & echo "\$!" | sudoIf tee \${SOCAT_PID} > /dev/null)
+        else
+            log "Socket proxy already running."
+        fi
+    fi
+    log "Success"
 fi
 
+# Execute whatever commands were passed in (if any). This allows us
+# to set this script to ENTRYPOINT while still executing the default CMD.
 set +e
 exec "\$@"
 EOF
 
-    chown ${_CONTAINER_USER}:root /usr/local/share/docker-init.sh
-fi
-
 chmod +x /usr/local/share/docker-init.sh
-
-if [[ $INSTALLDOCKERCOMPOSE == "true" ]]; then
-    apk --no-cache add docker-compose
-fi
-
+chown ${_REMOTE_USER}:root /usr/local/share/docker-init.sh
 
 
 echo 'Done!'
